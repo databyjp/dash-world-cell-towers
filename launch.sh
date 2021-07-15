f4eff71b0ee883370784ffa717b643fb500045d9
JP Hwang <me@jphwang.com>
2021-07-15 21:11:11 +0800

    changed launch.sh for dash gallery

--- launch.sh
+++ launch.sh
@@ -2,6 +2,10 @@
 # Add this directory to PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:`dirname "$(realpath $0)"`
 
+# For Dash-gallery
+pip install --upgrade pip # to the latest version
+pip install -r requirements-predeploy.txt  # Now, we can install them
+
 # # -- WHEN USING COILED - UNHIDE THE NEXT TWO LINES --
 python coiled_login.py  # Log into coiled using token saved as env variable
 python coiled_create_env.py  # Create coiled env up front
