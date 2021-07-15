c3fe181e9ff13f87bf8a1f75ec236479e0b1f5ae
JP Hwang <me@jphwang.com>
2021-07-15 21:20:51 +0800

    fixed app ref

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@ pip install -r requirements-predeploy.txt  # Now, we can install them
 # # -- WHEN USING COILED - UNHIDE THE NEXT TWO LINES --
 python coiled_login.py  # Log into coiled using token saved as env variable
 python coiled_create_env.py  # Create coiled env up front
-gunicorn "app:server" --timeout 240 --workers 2
+gunicorn "dash_opencellid.app:server" --timeout 240 --workers 2
 # -- END - COILED SECTION --
 
 # -- WHEN USING LOCAL COMPUTE - UNHIDE THE NEXT THREE LINES --
